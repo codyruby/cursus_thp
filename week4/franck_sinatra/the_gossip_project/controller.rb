0fcd36c7d1254fb5178a1eec5645dfb11394ae0c
Yousri <laifayousri@gmail.com>
2019-04-15 14:39:02 +0200

    modification de la route index

--- week4/franck_sinatra/the_gossip_project/controller.rb
+++ week4/franck_sinatra/the_gossip_project/controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < Sinatra::Base
     
   get '/' do
-    "<html><head><title>The Gossip Project</title></head><body><h1>Mon super site de gossip !</h1></body></html>"
+    erb :index
   end
 
 end
