aae3fe96d7c9008d29340273dc4784e4254aa324
Yousri <laifayousri@gmail.com>
2019-04-15 13:06:13 +0200

    ajout de commentaires

--- week4/franck_sinatra/hello_world_sinatra/app.rb
+++ week4/franck_sinatra/hello_world_sinatra/app.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
+# Route qui affiche la page /hello
 get '/hello' do
   '<h1>Hello world ! </h1>'
 end
 
+# Route qui affiche la page /hello
 get '/bonjour' do
     '<h1>Bonjour, monde !</h1>'
 end
