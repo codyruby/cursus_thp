97459578a37632c277b08b7f92573a77c377f624
Yousri <laifayousri@gmail.com>
2019-04-15 15:39:41 +0200

    ajout d'une méthode post pour l'envoi d'un new_gossip

--- week4/franck_sinatra/the_gossip_project/lib/controller.rb
+++ week4/franck_sinatra/the_gossip_project/lib/controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < Sinatra::Base
-    
+  
+  # Méthode get pour afficher la page index de notre application
   get '/' do
     erb :index
   end
 
+  # Méthode get pour afficher la page /gossips/new
   get '/gossips/new/' do
     erb :new_gossip
   end
 
+  # Méthode post pour l'envoi d'un formulaire"
   post '/gossips/new/' do
     puts "Ce programme ne fait rien pour le moment, on va donc afficher un message en console"
   end
