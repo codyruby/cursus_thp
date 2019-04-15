898180900d7e5d1dc5b54a52bebd5bff9bb180d5
Yousri <laifayousri@gmail.com>
2019-04-15 17:48:13 +0200

    ajout de commentaires

--- week4/franck_sinatra/the_gossip_project/lib/gossip.rb
+++ week4/franck_sinatra/the_gossip_project/lib/gossip.rb
@@ -1,10 +1,12 @@
 class Gossip
 
- def initialize(author, content)
+    # Méthode d'initialisation d'un instance Gossip
+    def initialize(author, content)
         @author = author
         @content = content
     end
 
+    # Méthode qui permet de sauvegarde la nouvelle instance Gossip dans le fichier csv
     def save
         CSV.open("./db/gossip.csv", "ab") do |csv|
           csv << ["#{@author}", "#{@content}"]
